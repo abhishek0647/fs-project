607b468d1afceabd879b93c53186a35b2c12fca9
Nehal Kumar <shona.nehal@gmail.com>
2014-05-21 17:56:56 +0530

    [Header] Fixed non-clickable links issue, removed active class for the time-being and added path for blog page

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
   get '/dashboard' => 'static_pages#dashboard'
   get '/contacts' => 'static_pages#contacts'
   get '/resources' => 'static_pages#resources'
+  get '/blog' => 'static_pages#blog'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
